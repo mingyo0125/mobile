029509737c425fbfa24cb53df56b9602e17c811f
mingyo0125 <wjdalsry125@gmail.com>
2024-10-08 23:59:29 +0900

    Edit Feedbacks

--- Assets/01.Scripts/Entity/EntityFeedback/Feedback.cs
+++ Assets/01.Scripts/Entity/EntityFeedback/Feedback.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 
 public abstract class Feedback : MonoBehaviour
 {
-    public abstract void PlayFeedback();
-    public abstract void StopFeedback();
+    public abstract void PlayFeedback<T, G>(IFeedbackPlayable<T, G> owner) where T : Enum where G : Entity<T,G>;
+    public abstract void StopFeedback<T, G>(IFeedbackPlayable<T, G> owner) where T : Enum where G : Entity<T, G>;
 }
--- Assets/01.Scripts/Entity/EntityFeedback/HitFeedback.cs
+++ Assets/01.Scripts/Entity/EntityFeedback/HitFeedback.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HitFeedback : Feedback
 {
-    public override void PlayFeedback()
+    public override void PlayFeedback<T, G>(IFeedbackPlayable<T, G> owner)
     {
+        Entity<T, G> entity = owner.GetEntity();
+
     }
 
-    public override void StopFeedback()
+    public override void StopFeedback<T, G>(IFeedbackPlayable<T, G> owner)
     {
     }
 }
