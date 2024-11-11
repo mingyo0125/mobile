092ded88a5cae837b898e83451089db5c83574ff
mingyo0125 <wjdalsry125@gmail.com>
2024-11-12 00:11:56 +0900

    Add Summon System

--- Assets/01.Scripts/UI/Summon/SkillSummonFactory.cs
+++ Assets/01.Scripts/UI/Summon/SkillSummonFactory.cs
@@ -5,5 +5,21 @@ using UnityEngine;
 public class SkillSummonFactory : SummonItemFactory<SkillInfo>
 {
     [SerializeField]
-    private SkillListSO _skillListSO;
+    private List<SkillInfoSO> _skillinfoList;
+
+    private void Start()
+    {
+        SpawnSummonItem(5);
+    }
+
+    public override List<SkillInfo> GetCanSummonItems()
+    {
+        List<SkillInfo> skills = new List<SkillInfo>();
+
+        foreach (SkillInfoSO item in _skillinfoList) // Test
+        {
+            skills.Add(item.SkillInfo);
+        }
+        return skills;
+    }
 }
--- Assets/01.Scripts/UI/Summon/SummonItemFactory.cs
+++ Assets/01.Scripts/UI/Summon/SummonItemFactory.cs
@@ -2,38 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SummonItemFactory<T> : ObjectFactory<SummonItem> where T : ISummonItem
+public abstract class SummonItemFactory<T> : ObjectFactory<SummonItem> where T : ISummonItem
 {
-    public virtual void SpawnSummonItem(, int count)
+    public abstract List<T> GetCanSummonItems();
+
+    public virtual void SpawnSummonItem(int count)
     {
-        for(int i = 0; i < count; i++)
+        List<T> summonedItem = GetSummonItems(GetCanSummonItems(), 10);
+
+        foreach(T item in summonedItem)
         {
-            T item = 
+            Debug.Log(item.GetSummonProbability());
         }
     }
 
-    private List<T> GetSummonItems(T[] cansummonItems, int count)
+    private List<T> GetSummonItems(List<T> cansummonItems, int count)
     {
         List<T> results = new List<T>();
 
+        float totalProbability = 0f;
+        foreach (var equipment in cansummonItems)
+        {
+            totalProbability += equipment.GetSummonProbability();
+        }
+
         for (int i = 0; i < count; i++)
         {
-            float totalProbability = 0f;
-            foreach (var equipment in cansummonItems)
-            {
-                totalProbability += equipment.GetSummonProbability();
-            }
-
             float randomPoint = Random.value * totalProbability;
 
-            foreach (var equipment in cansummonItems)
+            foreach (T item in cansummonItems)
             {
-                if (randomPoint < equipment.GetSummonProbability())
+                if (randomPoint < item.GetSummonProbability())
                 {
-                    results.Add(equipment);
+                    results.Add(item);
                     break;
                 }
-                randomPoint -= equipment.GetSummonProbability();
+                randomPoint -= item.GetSummonProbability();
             }
         }
 
