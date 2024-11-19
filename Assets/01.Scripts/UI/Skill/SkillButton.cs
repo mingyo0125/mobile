e1b957da351e932869ef007cb8e94150e3f6da4f
mingyo0125 <wjdalsry125@gmail.com>
2024-11-20 00:13:16 +0900

    Edit Skill Button Cooltime

--- Assets/01.Scripts/UI/Skill/SkillButton.cs
+++ Assets/01.Scripts/UI/Skill/SkillButton.cs
@@ -32,7 +32,7 @@ public class SkillButton : UI_Button
 
         UpdateUI();
         _button.onClick.RemoveAllListeners();
-        _button.onClick.AddListener(AddPermanentEvent);
+        _button.onClick.AddListener(() => StartCoroutine(CalculateSkillCooldownCorou()));
         _button.onClick.AddListener(() => _skillHolder.PlaySkill(skillID));
     }
 
@@ -48,28 +48,24 @@ public class SkillButton : UI_Button
         _skillButtonInfo = null;
     }
 
-    private void AddPermanentEvent()
-    {
-        //_button.onClick.AddListener(() => StartCoroutine(CalculateSkillCooldownCorou()));
-    }
-
     private IEnumerator CalculateSkillCooldownCorou()
     {
-        float goalTime = Time.time + _skillButtonInfo.CoolTime;
+        float cooldownTime = _skillButtonInfo.CoolTime; // 쿨타임 길이
+        float startTime = Time.time; // 시작 시간
+        float endTime = startTime + cooldownTime; // 종료 시간
 
         _button.interactable = false;
-
         _cooldownImage.fillAmount = 1;
 
-        while (Time.time < goalTime)
+        while (Time.time < endTime)
         {
-            _cooldownImage.fillAmount = 1 - Time.time / goalTime;
-
+            float elapsedTime = Time.time - startTime;
+            _cooldownImage.fillAmount = 1 - (elapsedTime / cooldownTime);
+            Debug.Log(1 - (elapsedTime / cooldownTime));
             yield return null;
         }
 
         _cooldownImage.fillAmount = 0;
         _button.interactable = true;
     }
-
 }
