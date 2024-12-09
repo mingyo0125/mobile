5416ee1c60a01fde1127bb81f196abcf8854c18f
mingyo0125 <wjdalsry125@gmail.com>
2024-12-09 20:24:46 +0900

    Edit Player Stat UI Visual

--- Assets/01.Scripts/UI/PlayerStat/PlayerStat_InfoUI.cs
+++ Assets/01.Scripts/UI/PlayerStat/PlayerStat_InfoUI.cs
@@ -9,7 +9,10 @@ public class PlayerStat_InfoUI : UI_Component
     private TextMeshProUGUI _statText;
 
     [SerializeField]
-    private EquipItem_Icon _equipItem_Icon;
+    private EquipItem_Icon _equippedItem_Icon;
+
+    [SerializeField]
+    private EquipItem_Icon[] _equippedSkills;  // 이게 맞나
 
     public override void UpdateUI()
     {
