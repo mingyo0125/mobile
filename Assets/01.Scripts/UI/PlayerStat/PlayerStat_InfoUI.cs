9fab0f99effce230da19da706559e8456ae1424f
mingyo0125 <wjdalsry125@gmail.com>
2024-12-09 20:16:56 +0900

    Edit Player Stat UI Visual

---
+++ Assets/01.Scripts/UI/PlayerStat/PlayerStat_InfoUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStat_InfoUI : UI_Component
+{
+    [SerializeField]
+    private TextMeshProUGUI _statText;
+
+    [SerializeField]
+    private EquipItem_Icon _equipItem_Icon;
+
+    public override void UpdateUI()
+    {
+
+    }
+}
